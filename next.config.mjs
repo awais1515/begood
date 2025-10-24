6b586f5e5ecb6579f9c3377e7995fbd8d3dd29d1
Fanni Preczner <fancsiangol@gmail.com>
2025-10-24 19:50:38 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : undefined,
   experimental: {
-    // This is required to allow the development environment to work correctly.
     allowedDevOrigins: [
       "https://9000-firebase-studio-1748527364540.cluster-oayqgyglpfgseqclbygurw4xd4.cloudworkstations.dev",
     ],
